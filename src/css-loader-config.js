b8f2cadd89fbfdcfdb90830a3a1c40e926d1317d
Chris <chrng2@gmail.com>
2020-04-22 20:15:02 -0700

    Rename hot to hmr to align with newer API

--- src/css-loader-config.js
+++ src/css-loader-config.js
@@ -42,7 +42,7 @@ module.exports = (
         chunkFilename: dev
           ? 'static/chunks/[name].chunk.css'
           : 'static/chunks/[name].[contenthash:8].chunk.css',
-        hot: dev,
+        hmr: dev,
         ignoreOrder: true
       })
     )
